4e5e1b1aa6d81efc83a98be8a5c773999727db41
LexionCoooooooooking <discordggggg4@gmail.com>
2025-07-10 15:32:47 +0200

    info-owner.js

--- plugins/info-owner.js
+++ plugins/info-owner.js
@@ -2,7 +2,7 @@ function handler(m) {
   const data = global.owner.filter(([id, isCreator]) => id && isCreator)
   const prova = { "key": {"participants":"0@s.whatsapp.net", "fromMe": false, "id": "Halo"
     }, "message": { 
-    "locationMessage": { name: 'Ꮻ𝐖𝐍𝚵𝐑    ꙰ 𝟥𝟥𝟥 ꙰ 𝔹𝕆𝕋  ꙰', "jpegThumbnail": fs.readFileSync('./icone/bal.png'),
+    "locationMessage": { name: 'Ꮻ𝐖𝐍𝚵𝐑', "jpegThumbnail": fs.readFileSync('./icone/bal.png'),
     "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
     }}, "participant": "0@s.whatsapp.net"}
   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), prova)
